4110043ec9685991d2894cf0cfe77bbcca9331ff
Kirill <freshjs.ftd.@gmail.com>
2022-05-26 11:00:12 +0300

    Refactor User model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const fs = require("fs").promises;
+const path = require('path');
+const fs = require('fs').promises;
+const { readFileOpts } = require('../config/db.json');
 
-const dbPath = path.resolve(__dirname, "../", "db/", "users.json");
-
+const dbPath = path.resolve(__dirname, '../', 'db/', 'users.json');
 class User {
   static async create(userData) {
-    const usersDataString = await fs.readFile(dbPath, "utf-8");
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
 
-    const users = JSON.parse(usersDataString);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
     const newUser = {
       ...userData,
@@ -19,7 +19,7 @@ class User {
     );
 
     if (existingUser) {
-      const error = new Error("User with this email already exists");
+      const error = new Error('User with this email already exists');
       error.code = 409;
       throw error;
     }
@@ -34,110 +34,94 @@ class User {
   }
 
   static async getAll() {
-    const usersDataString = await fs.readFile(dbPath, "utf-8");
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
 
-    const users = JSON.parse(usersDataString);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
     return users;
   }
 
   static async get(id) {
-    try {
-      const usersDataString = await fs.readFile(dbPath, "utf-8");
-      const users = JSON.parse(usersDataString);
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
 
-      const user = users.find((user) => user.id === +id);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
-      if (!users.length || !user) {
-        const error = new Error("User not found");
-        error.code = 404;
-        throw error;
-      }
+    const user = users.find((user) => user.id === +id);
 
-      return user;
-    } catch (error) {
+    if (!users.length || !user) {
+      const error = new Error('User not found');
+      error.code = 404;
       throw error;
     }
+
+    return user;
   }
 
   static async getByEmail(email) {
-    try {
-      const usersDataString = await fs.readFile(dbPath, "utf-8");
-      const users = JSON.parse(usersDataString);
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
-      const user = users.find((user) => {
-        console.log(user.email.toLowerCase());
-        console.log(email.toLowerCase());
-        const res = user.email.toLowerCase() === email.toLowerCase();
+    const user = users.find((user) => {
+      const res = user.email.toLowerCase() === email.toLowerCase();
 
-        console.log(res);
+      return res;
+    });
 
-        return res;
-      });
-
-      if (!users.length || !user) {
-        const error = new Error("User not found");
-        error.code = 404;
-        throw error;
-      }
-
-      return user;
-    } catch (error) {
+    if (!users.length || !user) {
+      const error = new Error('User not found');
+      error.code = 404;
       throw error;
     }
+
+    return user;
   }
 
   static async update(userData, id) {
-    try {
-      const usersDataString = await fs.readFile(dbPath, "utf-8");
-      const users = JSON.parse(usersDataString);
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
-      const userIndex = users.findIndex((user) => user.id === +id);
+    const userIndex = users.findIndex((user) => user.id === +id);
 
-      if (!users.length || userIndex === -1) {
-        const error = new Error("User not found");
-        error.code = 404;
-        throw error;
-      }
-
-      const newUsers = users.map((user, index) => {
-        const isSameUser = index === userIndex;
-
-        const updatedUser = isSameUser ? { ...user, ...userData } : { ...user };
-        return updatedUser;
-      });
-
-      const newUserData = JSON.stringify(newUsers, null, 2);
-
-      await fs.writeFile(dbPath, newUserData);
-
-      return updatedUsers[userIndex];
-    } catch (error) {
+    if (!users.length || userIndex === -1) {
+      const error = new Error('User not found');
+      error.code = 404;
       throw error;
     }
+
+    const newUsers = users.map((user, index) => {
+      const isSameUser = index === userIndex;
+
+      const updatedUser = isSameUser ? { ...user, ...userData } : { ...user };
+      return updatedUser;
+    });
+
+    const newUserData = JSON.stringify(newUsers, null, 2);
+
+    await fs.writeFile(dbPath, newUserData);
+
+    return updatedUsers[userIndex];
   }
 
   static async remove(id) {
-    try {
-      const usersDataString = await fs.readFile(dbPath, "utf-8");
-      const users = JSON.parse(usersDataString);
+    const usersDataString = await fs.readFile(dbPath, readFileOpts);
 
-      const userIndex = users.findIndex((user) => user.id === +id);
+    const users = JSON.parse(usersDataString ? usersDataString : '[]');
 
-      if (!users.length || userIndex === -1) {
-        const error = new Error("User not found");
-        error.code = 404;
-        throw error;
-      }
+    const userIndex = users.findIndex((user) => user.id === +id);
 
-      const newUsers = users.filter((user) => user.id !== +id);
-
-      const newUserData = JSON.stringify(newUsers, null, 2);
-
-      await fs.writeFile(dbPath, newUserData);
-    } catch (error) {
+    if (!users.length || userIndex === -1) {
+      const error = new Error('User not found');
+      error.code = 404;
       throw error;
     }
+
+    const newUsers = users.filter((user) => user.id !== +id);
+
+    const newUserData = JSON.stringify(newUsers, null, 2);
+
+    await fs.writeFile(dbPath, newUserData);
+
+    return id;
   }
 }
 
